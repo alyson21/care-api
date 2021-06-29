9f41e778f622865bf2b67af00342d0099f4abe8c
alyson.oliveira <alysonroliveira@hotmail.com>
2021-06-29 20:30:42 -0300

    initial commit

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+
+
+app.get('/', (req, res) => res.send('Servidor rodando') )
